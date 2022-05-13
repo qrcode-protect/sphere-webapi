d151efbd200d0f4d041b770b10850fad240a8d46
sakbly <sofiane.akbly@qrcode-protect.com>
2022-05-13 11:10:33 +0200

    Updated: Members unit crud test

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -5,9 +5,9 @@
  * file.
  */
 
-import type { Config } from '@japa/runner'
-import TestUtils from '@ioc:Adonis/Core/TestUtils'
-import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-adonis'
+import type { Config }                     from "@japa/runner"
+import TestUtils                           from "@ioc:Adonis/Core/TestUtils"
+import { apiClient, assert, specReporter } from "@japa/preset-adonis"
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-ad
 | Feel free to remove existing plugins or add more.
 |
 */
-export const plugins: Config['plugins'] = [assert(), runFailedTests(), apiClient()]
+export const plugins: Config["plugins"] = [ assert(), /*runFailedTests(),*/ apiClient() ]
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ export const plugins: Config['plugins'] = [assert(), runFailedTests(), apiClient
 | of tests on the terminal.
 |
 */
-export const reporters: Config['reporters'] = [specReporter()]
+export const reporters: Config["reporters"] = [ specReporter() ]
 
 /*
 |--------------------------------------------------------------------------
@@ -46,9 +46,9 @@ export const reporters: Config['reporters'] = [specReporter()]
 | within the runner hooks
 |
 */
-export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-  setup: [() => TestUtils.ace().loadCommands()],
-  teardown: [],
+export const runnerHooks: Required<Pick<Config, "setup" | "teardown">> = {
+    setup   : [ () => TestUtils.ace().loadCommands() ],
+    teardown: [],
 }
 
 /*
@@ -62,8 +62,8 @@ export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
 | You can use this method to configure suites. For example: Only start
 | the HTTP server when it is a functional suite.
 */
-export const configureSuite: Config['configureSuite'] = (suite) => {
-  if (suite.name === 'functional') {
-    suite.setup(() => TestUtils.httpServer().start())
-  }
+export const configureSuite: Config["configureSuite"] = (suite) => {
+    if (["functional", "unit"].includes(suite.name)) {
+        suite.setup(() => TestUtils.httpServer().start())
+    }
 }
