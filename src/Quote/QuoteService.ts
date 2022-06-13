758f257c7f8025ea278d8e182fb85552b5e67baf
sakbly <sofiane.akbly@qrcode-protect.com>
2022-06-13 16:22:51 +0200

    Updated: Accept / decline quotes

--- src/Quote/QuoteService.ts
+++ src/Quote/QuoteService.ts
@@ -19,10 +19,10 @@ import Application                      from "@ioc:Adonis/Core/Application";
 import { Bucket, GetSignedUrlResponse } from "@google-cloud/storage";
 import { Result }                       from "@sofiakb/adonis-response";
 import Log                              from "QRCP/Sphere/Common/Log";
-import DuplicateEntryException          from "QRCP/Sphere/Exceptions/DuplicateEntryException";
 import Config                           from "@ioc:Adonis/Core/Config";
 import moment                           from "moment";
 import { conversationModel }            from "App/Common/model";
+import Model                            from "QRCP/Sphere/Common/Model";
 
 interface StoreQuoteAttributes extends QuoteAttributes {
     upload?: unknown
@@ -59,21 +59,51 @@ export default class QuoteService extends Service {
         try {
             const result = await this.model.store({ ...data, transmitter })
             if (result.id) {
-                await conversationModel().updateMessage(data.conversationId, data.messageId, {
-                    attachmentID: result.id,
-                    attachment  : data.file
-                })
+                if (quote)
+                    await conversationModel().updateMessage(data.conversationId, data.messageId, {
+                        attachmentID: result.id,
+                        attachment  : data.file
+                    })
                 return Result.success(result)
             }
             throw new Error("Error while saving")
         } catch (e) {
             Log.error(e, true)
-            if (e instanceof DuplicateEntryException) {
-                return Result.duplicate("Un espace existe déjà avec cette adresse e-mail")
-            }
             return Result.error("Une erreur est survenue, merci de réessayer plus tard.")
         }
 
+    }
+
+    public async answer(docId: string, userId: string, accepted: boolean) {
+        try {
+            const quote = await this.model.whereSnapshot("customer", userId).whereSnapshot("id", docId).get()
+
+            if (quote === null) {
+                return Result.forbidden()
+            }
+
+            const result = await this.model.update(docId, {
+                accepted,
+                acceptedAt: accepted ? Model._now() : null,
+                declined  : !accepted,
+                declinedAt: !accepted ? Model._now() : null,
+            })
+            if (result.id) {
+                return Result.success(result)
+            }
+            throw new Error("Error while saving")
+        } catch (e) {
+            Log.error(e, true)
+            return Result.error("Une erreur est survenue, merci de réessayer plus tard.")
+        }
+    }
+
+    public async accept(docId: string, userId: string) {
+        return this.answer(docId, userId, true)
+    }
+
+    public async decline(docId: string, userId: string) {
+        return this.answer(docId, userId, false)
     }
 
 }
