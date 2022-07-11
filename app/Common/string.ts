02e2141263b0aa0bc9fc7d27fa553488a6be56bb
sakbly <sofiane.akbly@qrcode-protect.com>
2022-07-11 11:42:24 +0200

    --Updated-- Common: change number generation

--- app/Common/string.ts
+++ app/Common/string.ts
@@ -9,6 +9,8 @@
  * File app/Common/string
  */
 
+import { defaultDiacriticsRemovalMap } from "App/Common/diacritics";
+
 export const lower = (value: string): string => value?.toLowerCase();
 
 export const name = (value: string, replaceValue = "") => lower(value)?.replace(/[^A-Za-zàáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ-]/g, replaceValue)
@@ -19,6 +21,18 @@ export const reverse = (value: Nullable<string>) => value?.split("")?.reverse().
 
 export const capitalize = (value) => value?.charAt(0).toUpperCase() + value?.slice(1).toLowerCase();
 
-export const generateNumber = (lastname: string, phone: Nullable<string>, prefix?: string) => `${prefix ? prefix + "-": ""}00${lastname.toUpperCase().split("").reverse().join("").substring(0, 3)}${(phone ?? "XXXXXXXXXX").split("").reverse().join("").substring(0, 3)}`
+export const generateNumber = (count: number, prefix?: string) => `${prefix ? prefix + "-" : ""}${String(count + 1).padStart(6, "0")}`/*00${lastname.toUpperCase().split("").reverse().join("").substring(0, 3)}${(phone ?? "XXXXXXXXXX").split("").reverse().join("").substring(0, 3)}*/
 
 export const urlSingleBackslashes = (url: string) => url.replace(RegExp(/\/\//g), "/").replace("http:/", "http://").replace("https:/", "https://")
+
+export const stripAccents = (value: string) => {
+    return ((str: string) => {
+        const changes = defaultDiacriticsRemovalMap
+
+        for (let i = 0; i < changes.length; i++) {
+            str = str.replace(changes[i].letters, changes[i].base);
+        }
+
+        return str;
+    })(value)
+}
